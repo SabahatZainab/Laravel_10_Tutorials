6ec1a5de9a975defe051db9560dd18e8d0fee398
Sabahat Zainab <sabahatzainab65@gmail.com>
2024-01-23 22:21:21 +0500

    factory tutorial completed

---
+++ factory_tutorial/database/seeders/StudentSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Student::factory()->count(10)->create(); //both commends work similarly
+        Student::factory(10)->create();
+    }
+}
