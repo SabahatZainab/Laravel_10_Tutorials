2253156c8835c3fc3524715f1a7c33a0cc16a077
Sabahat Zainab <sabahatzainab65@gmail.com>
2024-01-23 16:53:34 +0500

    complete insert,update,delete query builder tutorial

--- query_builder/routes/web.php
+++ query_builder/routes/web.php
@@ -7,3 +7,7 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class,'showUsers'])->name('user');
 Route::get('/user/{id}', [UserController::class,'singleUser'])->name('view.user');
+Route::get('/add', [UserController::class,'addUser'])->name('add.user');
+Route::get('/update', [UserController::class,'updateUser'])->name('update.user');
+Route::get('/delete/{id}', [UserController::class,'deleteUser'])->name('delete.user');
+Route::get('/deleteAllUser', [UserController::class,'deleteAllUser']);
