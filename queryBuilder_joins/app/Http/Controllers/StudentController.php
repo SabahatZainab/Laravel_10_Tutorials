9e82ab4bc33224c2c13878c1cfbf838c6e32392c
Sabahat Zainab <sabahatzainab65@gmail.com>
2024-02-01 11:09:13 +0500

    join query builder topic completed

--- queryBuilder_joins/app/Http/Controllers/StudentController.php
+++ queryBuilder_joins/app/Http/Controllers/StudentController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function showStudents(){
+
         $students = DB::table('students')
-                        ->join('cities','students.city','=','cities.id')
-                        ->get();
+                            ->leftJoin('cities',function(JoinClause $join){
+                                $join->on('students.city','=','cities.id')
+                                    ->where('cities.city_name','=','City1');
+                            })
+                            ->select('students.*','cities.city_name')
+                            ->get();
 
         // return $students;
 
